d5576e5fde1ef87163d3b1ebede4b756924eced1
raloliver <contato@raloliver.com.br>
2021-06-27 09:39:28 +0100

    init project

---
+++ src/main/java/com/raloliver/sbcd3/SbCd3Application.java
@@ -0,0 +1,13 @@
+package com.raloliver.sbcd3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCd3Application.class, args);
+	}
+
+}
